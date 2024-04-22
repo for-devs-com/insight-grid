b1a97bccef38bbe522016d8d32d22d373042976a
Enoc Velásquez <fungicoder@gmail.com>
2024-04-21 23:54:15 -0600

    reorganized with project organization strategy by next.js

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -3,13 +3,15 @@
 import {Container} from "@mui/material";
 import SelectInput from "@mui/material/Select/SelectInput";
 import {AppRouterCacheProvider} from "@mui/material-nextjs/v14-appRouter";
+import Dashboard from "@/app/dashboard/components/Dashboard";
 
-export default function DbConnPage(children) {
+export default function DashboardPage(children) {
 
     return <AppRouterCacheProvider>
                 <Container>
                     <h1>Grid Page </h1>
                     <p>Success Connection to the Intelligent data insight tool for your business</p>
+                    <Dashboard>HI</Dashboard>
                 </Container>
      </AppRouterCacheProvider>
 
