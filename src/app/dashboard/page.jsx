958fc37f09661326010b09a4f7aae8be518fc694
Enoc Velásquez <fungicoder@gmail.com>
2024-04-24 01:54:06 -0600

    optimized imports

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -1,7 +1,6 @@
 'use client'
 
 import {Container} from "@mui/material";
-import SelectInput from "@mui/material/Select/SelectInput";
 import {AppRouterCacheProvider} from "@mui/material-nextjs/v14-appRouter";
 import Dashboard from "@/app/dashboard/components/Dashboard";
 
