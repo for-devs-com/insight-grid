c46667e27d9ad6caca36351213a33af60beb4c2d
Enoc.Velza(Fungicoder) <fungicoder@gmail.com>
2024-06-06 15:24:24 -0600

    set react dependency to the Providers.tsx

--- src/app/Providers.tsx
+++ src/app/Providers.tsx
@@ -1,5 +1,6 @@
-"use client";
+"use client"
 import { SessionProvider } from "next-auth/react";
+import React from "react";
 
 export function Providers({ children }: { children: React.ReactNode }) {
     return <SessionProvider>{children}</SessionProvider>;
