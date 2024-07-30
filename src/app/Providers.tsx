f5128fa7d78d25b6832fb40920660cf058841bc3
Enoc.Velza(Fungicoder) <fungicoder@gmail.com>
2024-07-30 16:15:31 -0600

    migrating styles from MaterialUi to TailwindCSS

--- src/app/Providers.tsx
+++ src/app/Providers.tsx
@@ -1,7 +1,12 @@
-"use client"
-import { SessionProvider } from "next-auth/react";
-import React from "react";
+'use client';
+import {SessionProvider} from "next-auth/react";
+import React from 'react';
+import {ThemeProvider} from "@/components/theme-provider";
 
-export function Providers({ children }: { children: React.ReactNode }) {
-    return <SessionProvider>{children}</SessionProvider>;
+export function Providers({children}: { children: React.ReactNode }) {
+    return <SessionProvider>
+        <ThemeProvider>
+                {children}
+        </ThemeProvider>
+    </SessionProvider>;
 }
--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,20 +1,17 @@
 "use client"
 
-import {Container} from "@mui/material";
-import {AppRouterCacheProvider} from "@mui/material-nextjs/v14-appRouter";
 import DynamicTables from "@/components/DynamicTables";
 import React from "react";
 
 export default function DashboardPage() {
 
     return (
-        <AppRouterCacheProvider>
-            <Container>
-                <h1>Grid Page </h1>
-                <p>Success Connection to the Intelligent data insight tool for your business</p>
-                <DynamicTables/>
-            </Container>
-        </AppRouterCacheProvider>);
+        <div>
+            <h1>Grid Page </h1>
+            <p>Success Connection to the Intelligent data insight tool for your business</p>
+            <DynamicTables/>
+        </div>
+    );
 
 
 }
