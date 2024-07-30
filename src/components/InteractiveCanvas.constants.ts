b01eb2429c187d5cdd92adc46180c2df71157a46
Enoc.Velza(Fungicoder) <fungicoder@gmail.com>
2024-07-30 16:38:02 -0600

    Created InteractiveCanvas.constants.ts

--- src/components/InteractiveCanvas.constants.ts
+++ src/components/InteractiveCanvas.constants.ts
@@ -1,21 +1,28 @@
-import {Edge, Node} from "reactflow";
+import { AppNode } from "@/components/nodes/types";
+import { Edge } from "@xyflow/react";
+import DatabaseConnectionNode from "@/components/nodes/DatabaseConnectionNode";
+import InputNode from "@/components/nodes/InputNode";
 
+
+const position = { x: Math.random() * 250, y: Math.random() * 250 };
 export const initialEdges: Edge[] = [];
 
-export const initialNodes: Node[] = [
+export const initialNodes: AppNode[] = [
     {
         id: '1',
-        data: {label: 'FilterEngine'},
-        position: {x: 0, y: 0},
+        type: 'databaseConnection',
+        position,
+        data: { label: 'Database Connection' },
     },
     {
         id: '2',
-        data: {label: 'Ouput'},
-        position: {x: 100, y: 100},
-    },
-    {
-        id: '3',
-        data: {label: 'Operations'},
-        position: {x: 200, y: 200},
+        type: 'inputNode',
+        position,
+        data: { label: 'Input' },
     },
 ];
+
+export const nodeTypes = {
+    databaseConnection: DatabaseConnectionNode,
+    inputNode: InputNode,
+};
