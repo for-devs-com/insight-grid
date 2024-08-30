bb7ec5e13a4422f3207465f0ede90394d45b2c44
Enoc.Velza(Fungicoder) <fungicoder@gmail.com>
2024-08-30 09:35:50 -0600

    update session validation on dashboard page

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -2,9 +2,16 @@
 
 import DynamicTables from "@/components/DynamicTables";
 import React from "react";
+import {useSession} from "next-auth/react";
 
 export default function DashboardPage() {
-
+    const {data: session, status}  = useSession();
+    if (status === 'loading') {
+        return <div>Loading...</div>
+    }
+    if (status === 'unauthenticated') {
+        return <div>Access Denied</div>
+    }
     return (
         <div>
             <h1>Grid Page </h1>
