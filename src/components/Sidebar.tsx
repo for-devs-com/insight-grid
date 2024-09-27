4e72fe11a35d2634a2a8351126851d3be6f6760d
Enoc.Velza(Fungicoder) <fungicoder@gmail.com>
2024-09-27 00:51:38 -0600

    Add Sidebar.tsx props and logic

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,20 +1,27 @@
+'use client';
 
-export default function Sidebar() {
+interface SidebarProps {
+    className?: string;
+}
+export default function Sidebar({className}: SidebarProps) {
     return (
         <div className="flex flex-col w-64 h-screen bg-gray-800">
             <div className="flex items-center justify-center h-16 border-b border-gray-900">
-                <h1 className="text-white text-2xl">Data Analytics</h1>
+                <h1 className="text-white text-2xl">Data Analytic</h1>
             </div>
             <div className="p-4">
                 <ul className="space-y-2">
                     <li>
-                        <a href="#" className="block text-white">Dashboard</a>
+                        <a href="/dashboard" className="block text-white">Dashboard</a>
+                    </li>
+                    <li>
+                        <a href="/data_explorer" className="block text-white">Data Explorer</a>
+                    </li>
+                    <li>
+                        <a href="/data_transfer" className="block text-white">Easy Data Transfer</a>
                     </li>
                     <li>
                         <a href="#" className="block text-white">Reports</a>
-                    </li>
-                    <li>
-                        <a href="#" className="block text-white">Esasy Data Transfer</a>
                     </li>
                     <li>
                         <a href="#" className="block text-white">Settings</a>
