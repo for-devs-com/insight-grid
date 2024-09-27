88d20bd9c85798b7c4fb6d78601488d23f717597
Enoc.Velza(Fungicoder) <fungicoder@gmail.com>
2024-09-27 00:54:44 -0600

    add page.tsx for data_transfer route

---
+++ src/app/data_transfer/page.tsx
@@ -0,0 +1,10 @@
+
+export default function Page() {
+    return (
+        <div className="flex flex-1">
+            <div className="flex flex-1">
+                <h1 className="text-2xl font-bold mb-2">Data Transfer</h1>
+            </div>
+        </div>
+    );
+}
