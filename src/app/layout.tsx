cf649495bed03d4218a2f08560abbe52b99ae50d
Enoc.Velza(Fungicoder) <fungicoder@gmail.com>
2024-09-27 00:59:32 -0600

    update root layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,29 +1,36 @@
-'use client';
-
-import {Inter} from "next/font/google";
-import {Providers} from "@/components/Providers";
+import { Inter } from "next/font/google";
+import { Providers } from "@/components/Providers";
 import Navbar from "@/components/Navbar";
-import React from "react";
 import "@/app/globals.css";
+import Sidebar from "@/components/Sidebar";
+import {metadata}  from "@/app/metadata";
 
-const inter = Inter({subsets: ["latin"]});
+const inter = Inter({ subsets: ["latin"] });
 
-export default function RootLayout({children}) {
+const title = metadata.title;
+const description = metadata.description;
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
     return (
-        <html lang="en">
-        <head>
-            <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-            <title>Data Analytics</title>
-        </head>
-        <body className={`${inter.className} bg-gray-50 text-gray-900 w-full`}>
+        <html lang="en" className="h-full">
+        <body className={`${inter.className} bg-gray-50 text-gray-900 w-full h-full`}>
+        {/* Proveedores globales */}
         <Providers>
-            {/* Main Layout */}
-            <div className="flex-1 h-full " >
-                {/* Navbar */}
-                <Navbar/>
-                <main className="flex-grow">
-                    {children}
-                </main>
+            {/* Contenedor principal que ocupa toda la pantalla */}
+            <div className="flex flex-col h-full">
+                {/* Navbar en la parte superior */}
+                <Navbar />
+
+                {/* Contenido principal con Sidebar y contenido dinámico */}
+                <div className="flex flex-1 overflow-hidden">
+                    {/* Sidebar fijo en el lado izquierdo */}
+                    <Sidebar className="w-64 h-full"/>
+
+                    {/* Contenido dinámico */}
+                    <main className="flex-1 overflow-auto">
+                        {children}
+                    </main>
+                </div>
             </div>
         </Providers>
         </body>
