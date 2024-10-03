dc45cbbf78ec5a38812b7c32649c43d9299e7f2f
Enoc.Velza(Fungicoder) <fungicoder@gmail.com>
2024-10-03 10:06:45 -0600

    fix ContentArea.tsx  height and width

--- src/components/ContentArea.tsx
+++ src/components/ContentArea.tsx
@@ -12,9 +12,9 @@ const ContentArea: React.FC<ContentAreaProps> = ({ children }) => {
 
     return (
         <main
-            className={`flex-1 overflow-auto transition-all duration-300 ease-in-out ${
-                isSidebarCollapsed ? 'ml-64' : 'ml-0'
-            }`}
+            className={`w-full overflow-auto transition-all duration-300 ease-in-out ${
+                isSidebarCollapsed ? 'ml-0' : 'ml-0'
+            } p-0`}
         >
             {children}
         </main>
