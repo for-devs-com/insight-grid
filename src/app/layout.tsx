bdba37a35f3dfe6c24d1872b0ae97781a793bfc5
Enoc.Velza(Fungicoder) <fungicoder@gmail.com>
2024-10-03 10:11:47 -0600

    update the root layout.tsx at app/ & at app/data_explorer route for properly screen fit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,12 @@
-
 import { Inter } from "next/font/google";
 import { Providers } from "@/components/Providers";
 import Navbar from "@/components/Navbar";
 import "@/app/globals.css";
 import Sidebar from "@/components/Sidebar";
-import {Metadata} from 'next';
-import ContentArea from "@/components/ContentArera";
+import ContentArea from "@/components/ContentArea";
+import { Metadata } from 'next';
 
 const inter = Inter({ subsets: ["latin"] });
-
 
 export const metadata: Metadata = {
     title: 'Data Analytic',
@@ -24,18 +22,18 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
     return (
         <html lang="en" className="h-full">
-        <body className={`${inter.className} bg-gray-50 text-gray-900 w-full h-full`}>
+        <body className={`${inter.className} w-full h-full`}>
         {/* Proveedores globales */}
         <Providers>
             {/* Contenedor principal que ocupa toda la pantalla */}
-            <div className="flex flex-col h-full">
+            <div className="flex flex-col h-full w-full">
                 {/* Navbar en la parte superior */}
                 <Navbar />
 
                 {/* Contenido principal con Sidebar y contenido dinámico */}
-                <div className="flex flex-1 overflow-hidden relative">
+                <div className="flex flex-1 overflow-hidden">
                     {/* Sidebar */}
-                    <Sidebar/>
+                    <Sidebar className="h-full" />
 
                     {/* Contenido dinámico */}
                     <ContentArea>{children}</ContentArea>
